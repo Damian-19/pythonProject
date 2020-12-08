65967d10c1106752f20b17c8392f5dc86d0f785a
Damian <18230253@studentmail.ul.ie>
2020-12-08 14:46:33 +0000

    Updated conversion script

--- conversion.py
+++ conversion.py
@@ -1,18 +1,25 @@
 # ------------------------------------------------------------------
-# File name       : main.py
+# File name       : conversion.py
 # ------------------------------------------------------------------
 # Group number    : 1
 # Group members   : Damian Larkin (18230253) & James Cusack (18250416)
-# Last updated on : 02/12/2020
+# Last updated on : 08/12/2020
+# Adapted from python script by Wasim Ghazal Aswad (17193559)
 # ------------------------------------------------------------------
 # Module description:
 #   python script to satisfy project requirements for ET4244 Autumn
 #   semester. Converts .txt experiment file to .csv format
 # ------------------------------------------------------------------
 
+# global declarations
 global file1, file2
 
 
+# ------------------------------------------------------------------
+# def open_files(experiment_file)
+# ------------------------------------------------------------------
+# opens given experiment file & csv output file
+# ------------------------------------------------------------------
 def open_files(experiment_file):
     csv_file_output = 'csv/output.csv'
 
@@ -35,137 +42,110 @@ def convert(experiment_file):
     global file1, file2
     lines = file2.readlines()
     for line in lines:
-        if 'Sample' in line:
+        if 'Sample' in line:  # check if line contains 'Sample'
             i += 1
             file1.write("\n" + str(i))
-        elif 'Accelerometer' in line:
+
+        elif 'Accelerometer' in line:  # check if line contains 'Accelerometer'
             for row in line:
                 x = ','.join(row)
                 for number in range(0, len(x)):
                     text = (x[number])
-                    # check text = , or - or .
                     if text == ',' or text == '-' or text == '.':
-                        # add text to list
                         empty_list.append(text)
 
                     elif not text.isnumeric() and not text.isdigit() \
                             and not text.isalpha():
                         if not text == '<' and not text == '>' and not text == ']':
                             if not text == '[' and not text == ' ' and not text == '=':
-                                # check if the text is not \n (new line)
                                 if not text == '\n':
-                                    # add the text to the empty list just to be sure it will add just the
-                                    # Comma and Negative sign then it will add to empty list
                                     empty_list.append(text)
 
-                    # check if the text is number
-                    elif text.isnumeric():
-                        # add text to list
+                    elif text.isnumeric():  # check if the text is number
                         empty_list.append(text)
 
-        # To check if it was there 'Temperature' word
-        elif 'Temperature' in line:
-            # Read first row from the file
+        elif 'Temperature' in line:  # check if line contains 'Temperature'
             for row in line:
-                # Store each object in the variable X
                 x = ','.join(row)
                 for number in range(0, len(x)):
                     text = (x[number])
-                    # check if the text = , or - or .
                     if text == ',' or text == '-' or text == '.':
-                        # add the text to the empty list
                         empty_list.append(text)
-                    # check if the text not number or alphabet
-                    elif not text.isnumeric() and not text.isdigit() \
-                            and not text.isalpha():
-                        # check if the text not < or > or ]
-                        if not text == '<' and not text == '>' and not text == ']':
-                            # check if the text not space or [ or =
-                            if not text == '[' and not text == ' ' and not text == '=':
-                                # check if the text is not \n (new line)
-                                if not text == '\n':
-                                    # add the text to the empty list just to be sure it will add just the
-                                    # Comma and Negative sign then it will add to empty list
-                                    empty_list.append(text)
-                    # check if the text is number
-                    elif text.isnumeric():
-                        # add the text to the empty list
-                        empty_list.append(text)
-        # To check if it was there 'Gyroscope' word
-        elif 'Gyroscope' in line:
-            # Read first row from the file
-            for row in line:
-                # Store each object in the variable X
-                x = ','.join(row)
-                for number in range(0, len(x)):
-                    text = (x[number])
-                    # check if the text = , or - or .
-                    if text == ',' or text == '-' or text == '.':
-                        # add the text to the empty list
-                        empty_list.append(text)
-                    # check if the text not number or alphabet
-                    elif not text.isnumeric() and not text.isdigit() \
-                            and not text.isalpha():
-                        # check if the text not < or > or ]
-                        if not text == '<' and not text == '>' and not text == ']':
-                            # check if the text not space or [ or =
-                            if not text == '[' and not text == ' ' and not text == '=':
-                                # check if the text is not \n (new line)
-                                if not text == '\n':
-                                    # add the text to the empty list just to be sure it will add just the
-                                    # Comma and Negative sign then it will add to empty list
-                                    empty_list.append(text)
-                    # check if the text is number
-                    elif text.isnumeric():
-                        # add the text to the empty list
-                        empty_list.append(text)
-        # To check if it was there 'Pitch' word
-        elif 'Pitch' in line:
-            # Read first row from the file
-            for row in line:
-                # Store each object in the variable X
-                x = ','.join(row)
-                for number in range(0, len(x)):
-                    text = (x[number])
-                    # check if the text = , or - or .
-                    if text == ',' or text == '-' or text == '.':
-                        # add the text to the empty list
-                        empty_list.append(text)
-                    # check if the text not number or alphabet
-                    elif not text.isnumeric() and not text.isdigit() \
-                            and not text.isalpha():
-                        # check if the text not < or > or ]
-                        if not text == '<' and not text == '>' and not text == ']':
-                            # check if the text not space or [ or =
-                            if not text == '[' and not text == ' ' and not text == '=':
-                                # check if the text is not \n (new line)
-                                if not text == '\n':
-                                    # add the text to the empty list just to be sure it will add just the
-                                    # Comma and Negative sign then it will add to empty list
-                                    empty_list.append(text)
-                    # check if the text is number
-                    elif text.isnumeric():
-                        # add the text to the empty list
-                        empty_list.append(text)
-        # check if the empty_list is not empty
 
-        if empty_list:
+                    elif not text.isnumeric() and not text.isdigit() \
+                            and not text.isalpha():
+                        if not text == '<' and not text == '>' and not text == ']':
+                            if not text == '[' and not text == ' ' and not text == '=':
+                                if not text == '\n':
+                                    empty_list.append(text)
+
+                    elif text.isnumeric():  # check if the text is number
+                        empty_list.append(text)
+
+        elif 'Gyroscope' in line:  # check if line contains 'Gyroscope'
+            for row in line:
+                x = ','.join(row)
+                for number in range(0, len(x)):
+                    text = (x[number])
+                    if text == ',' or text == '-' or text == '.':
+                        empty_list.append(text)
+
+                    elif not text.isnumeric() and not text.isdigit() \
+                            and not text.isalpha():
+                        if not text == '<' and not text == '>' and not text == ']':
+                            if not text == '[' and not text == ' ' and not text == '=':
+                                if not text == '\n':
+                                    empty_list.append(text)
+
+                    elif text.isnumeric():  # check if the text is number
+                        empty_list.append(text)
+
+        elif 'Pitch' in line:  # check if line contains 'Pitch
+            for row in line:
+                x = ','.join(row)
+                for number in range(0, len(x)):
+                    text = (x[number])
+                    if text == ',' or text == '-' or text == '.':
+                        empty_list.append(text)
+
+                    elif not text.isnumeric() and not text.isdigit() \
+                            and not text.isalpha():
+                        if not text == '<' and not text == '>' and not text == ']':
+                            if not text == '[' and not text == ' ' and not text == '=':
+                                if not text == '\n':
+                                    empty_list.append(text)
+
+                    elif text.isnumeric():  # check if the text is number
+                        empty_list.append(text)
+
+        if empty_list:  # check list is not empty
             # convert the list to string
             # https://www.geeksforgeeks.org/python-program-to-convert-a-list-to-string/
             print_list_to_file = ''.join(map(str, empty_list))
-            # write the string print_list_to_file to the file
-            file1.write(print_list_to_file)
-            # delete all the element on the list
-            empty_list = []
+            file1.write(print_list_to_file)  # write list to file
+            empty_list = []  # clear list
 
     file1.close()
     file2.close()
     print('program end')
 
 
+# ------------------------------------------------------------------
+# def main()
+# ------------------------------------------------------------------
+# main function
+# ------------------------------------------------------------------
 def main():
     convert()
 
 
+# ------------------------------------------------------------------
+# Module is only run as a script or with python -m,  but not
+# when it is imported.
+# ------------------------------------------------------------------
 if __name__ == '__main__':
     main()
+
+# ------------------------------------------------------------------
+# End of script
+# ------------------------------------------------------------------
